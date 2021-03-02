fe42e9819b85c1f0a137f41235b6b985516d715a
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-02 16:02:40 +1100

    Generate an approximation of classes, though missing submessages

--- internal/codegen/run.go
+++ internal/codegen/run.go
@@ -54,15 +54,40 @@ func generateAllFiles(request *pluginpb.CodeGeneratorRequest) (outfiles []*plugi
 }
 
 func generateFullFile(f *descriptorpb.FileDescriptorProto) (out *pluginpb.CodeGeneratorResponse_File, err error) {
+	if f.GetSyntax() != "proto3" {
+		err = fmt.Errorf("proto3 is the only syntax supported by protoc-gen-tsjson, found %s in %s", f.GetSyntax(), f.GetName())
+		return
+	}
 	parsedName := filenameFromProto(f.GetName())
 	out = &pluginpb.CodeGeneratorResponse_File{
 		Name: proto.String(parsedName.fullWithoutExtension + ".ts"),
 	}
 	content := &strings.Builder{}
 	content.WriteString(getCodeGenmarker(version.GetVersionString(), protocVersion, f.GetName()))
+	// Imports
+	generateDependencies(f.GetDependency(), content)
+	// Enums
 	generateEnums(f.GetEnumType(), content)
+	// Messages
+	generateMessages(f.GetMessageType(), content)
+	// Services
+	generateServices(f.GetService(), content)
+	// Comments? unclear how to link them back to other elements
+	generateComments(f.GetSourceCodeInfo(), content)
 	out.Content = proto.String(content.String())
 	return
+}
+
+func generateDependencies(deps []string, content *strings.Builder) {
+	// FIXME: this pattern probably doesn't work at all for deps, I think we need to return structs that get imports added over time
+	// for _, dep := range deps {
+	// TODO: get comment data somehow
+	// 	comment := ""
+	// 	name := filenameFromProto(dep)
+	// 	content.WriteString(fmt.Sprintf("/** %s */\nimport {} from \"%s\";\n", comment, name.fullWithoutExtension))
+	// }
+	// content.WriteString("\n")
+	content.WriteString("// TODO: imports\n\n")
 }
 
 func generateEnums(enums []*descriptorpb.EnumDescriptorProto, content *strings.Builder) {
@@ -83,4 +108,63 @@ func generateEnums(enums []*descriptorpb.EnumDescriptorProto, content *strings.B
 		}
 		content.WriteString("}\n\n")
 	}
+}
+
+func generateMessages(messages []*descriptorpb.DescriptorProto, content *strings.Builder) {
+	for _, message := range messages {
+		// TODO: get comment data somehow
+		comment := "A message"
+		content.WriteString(fmt.Sprintf("/** %s */\nexport class %s {\n", comment, message.GetName()))
+		var tsType string
+		var tsDefault string
+		for _, field := range message.GetField() {
+			switch field.GetType() {
+			case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE,
+				descriptorpb.FieldDescriptorProto_TYPE_FLOAT,
+				descriptorpb.FieldDescriptorProto_TYPE_INT64,
+				descriptorpb.FieldDescriptorProto_TYPE_UINT64,
+				descriptorpb.FieldDescriptorProto_TYPE_INT32,
+				descriptorpb.FieldDescriptorProto_TYPE_FIXED64,
+				descriptorpb.FieldDescriptorProto_TYPE_FIXED32,
+				descriptorpb.FieldDescriptorProto_TYPE_UINT32,
+				descriptorpb.FieldDescriptorProto_TYPE_SFIXED32,
+				descriptorpb.FieldDescriptorProto_TYPE_SFIXED64,
+				descriptorpb.FieldDescriptorProto_TYPE_SINT32,
+				descriptorpb.FieldDescriptorProto_TYPE_SINT64:
+				// Javascript only has one number format
+				tsType = "number"
+				tsDefault = "0"
+			case descriptorpb.FieldDescriptorProto_TYPE_BOOL:
+				tsType = "boolean"
+				tsDefault = "false"
+			case descriptorpb.FieldDescriptorProto_TYPE_STRING:
+				tsType = "string"
+				tsDefault = `""`
+			case descriptorpb.FieldDescriptorProto_TYPE_BYTES:
+				tsType = "Uint8Array"
+				tsDefault = "new Uint8Array(0)"
+			case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+				// TODO: special case
+				tsType = "undefined"
+				tsDefault = "undefined"
+			case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
+				// TODO: special case
+				tsType = "undefined"
+				tsDefault = "undefined"
+			}
+			// TODO: get comment data somehow
+			comment = "A field"
+			content.WriteString(fmt.Sprintf("	/** %s */\n	public %s: %s = %s;\n", comment, field.GetName(), tsType, tsDefault))
+		}
+		message.GetNestedType()
+		content.WriteString("}\n\n")
+	}
+}
+
+func generateServices(services []*descriptorpb.ServiceDescriptorProto, content *strings.Builder) {
+
+}
+
+func generateComments(sourceCodeInfo *descriptorpb.SourceCodeInfo, content *strings.Builder) {
+
 }
