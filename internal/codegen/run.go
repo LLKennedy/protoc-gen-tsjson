9b60457094a1414c8e6cd1b17d87d8f717fdeb08
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-13 23:23:05 +1100

    Imports all appear to be working

--- internal/codegen/run.go
+++ internal/codegen/run.go
@@ -2,6 +2,7 @@ package codegen
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/LLKennedy/protoc-gen-tsjson/internal/version"
@@ -12,11 +13,21 @@ import (
 
 var protocVersion = "unknown"
 
+var packageReplacement = regexp.MustCompile(`\.([a-zA-Z0-9_]+)\.(.*)`)
+
 // At time of writing, the only feature that can be marked as supported is restoring the "optional" keyword to proto3, still an experimental feature that is out of scope for this project.
 var support uint64 = uint64(pluginpb.CodeGeneratorResponse_FEATURE_NONE)
 
 // Run performs code generation on the input data
 func Run(request *pluginpb.CodeGeneratorRequest) (response *pluginpb.CodeGeneratorResponse) {
+	defer func() {
+		if r := recover(); r != nil {
+			response = &pluginpb.CodeGeneratorResponse{
+				SupportedFeatures: &support,
+				Error:             proto.String(fmt.Sprintf("caught panic in protoc-gen-tsjson: %v", r)),
+			}
+		}
+	}()
 	// Set runtime version of protoc
 	protocVersion = version.FormatProtocVersion(request.GetCompilerVersion())
 	// Create a basic response with our feature support (none, see above)
@@ -43,8 +54,14 @@ func Run(request *pluginpb.CodeGeneratorRequest) (response *pluginpb.CodeGenerat
 // Naive approach to codegen, creates output files for every message/service in every linked file, not just the parts depended on by the "to generate" files
 func generateAllFiles(request *pluginpb.CodeGeneratorRequest) (outfiles []*pluginpb.CodeGeneratorResponse_File, err error) {
 	var out *pluginpb.CodeGeneratorResponse_File
+	var pkgMap map[string]string
+	outfiles, pkgMap, err = generatePackages(request)
+	if err != nil {
+		outfiles = nil
+		return
+	}
 	for _, file := range request.GetProtoFile() {
-		out, err = generateFullFile(file)
+		out, err = generateFullFile(file, pkgMap)
 		if err != nil {
 			return
 		}
@@ -53,7 +70,50 @@ func generateAllFiles(request *pluginpb.CodeGeneratorRequest) (outfiles []*plugi
 	return
 }
 
-func generateFullFile(f *descriptorpb.FileDescriptorProto) (out *pluginpb.CodeGeneratorResponse_File, err error) {
+func generatePackages(request *pluginpb.CodeGeneratorRequest) (out []*pluginpb.CodeGeneratorResponse_File, pkgMap map[string]string, err error) {
+	pkgMap = make(map[string]string)
+	packageNames := map[string][]string{}
+	for _, file := range request.GetProtoFile() {
+		if file.GetSyntax() != "proto3" {
+			err = fmt.Errorf("proto3 is the only syntax supported by protoc-gen-tsjson, found %s in %s", file.GetSyntax(), file.GetName())
+			return
+		}
+		pkgName := file.GetPackage()
+		if pkgName == "" {
+			err = fmt.Errorf("packages are mandatory with protoc-gen-tsjson, %s did not have a package", file.GetName())
+			return
+		}
+		if pkgName == "index" {
+			err = fmt.Errorf("for JS/TS language reasons, \"index\" is an invalid package name")
+		}
+		pkgMap[file.GetName()] = pkgName
+		list, _ := packageNames[pkgName]
+		list = append(list, file.GetName())
+		packageNames[pkgName] = list
+	}
+	indexFile := &pluginpb.CodeGeneratorResponse_File{
+		Name: proto.String("__packages__/index.ts"),
+	}
+	indexContent := &strings.Builder{}
+	for pkgName, importList := range packageNames {
+		outFile := &pluginpb.CodeGeneratorResponse_File{
+			Name: proto.String(fmt.Sprintf("__packages__/%s.ts", pkgName)),
+		}
+		content := &strings.Builder{}
+		for _, importFile := range importList {
+			parsedName := filenameFromProto(importFile)
+			content.WriteString(fmt.Sprintf("export * from \"%s\";\n", parsedName.fullWithoutExtension))
+		}
+		outFile.Content = proto.String(content.String())
+		out = append(out, outFile)
+		indexContent.WriteString(fmt.Sprintf("export * as %s from \"__packages__/%s\";\n", pkgName, pkgName))
+	}
+	indexFile.Content = proto.String(indexContent.String())
+	out = append(out, indexFile)
+	return
+}
+
+func generateFullFile(f *descriptorpb.FileDescriptorProto, pkgMap map[string]string) (out *pluginpb.CodeGeneratorResponse_File, err error) {
 	if f.GetSyntax() != "proto3" {
 		err = fmt.Errorf("proto3 is the only syntax supported by protoc-gen-tsjson, found %s in %s", f.GetSyntax(), f.GetName())
 		return
@@ -65,11 +125,11 @@ func generateFullFile(f *descriptorpb.FileDescriptorProto) (out *pluginpb.CodeGe
 	content := &strings.Builder{}
 	content.WriteString(getCodeGenmarker(version.GetVersionString(), protocVersion, f.GetName()))
 	// Imports
-	generateDependencies(f.GetDependency(), content)
+	generateDependencies(f.GetDependency(), pkgMap, content)
 	// Enums
 	generateEnums(f.GetEnumType(), content)
 	// Messages
-	generateMessages(f.GetMessageType(), content)
+	generateMessages(f.GetMessageType(), content, f.GetPackage())
 	// Services
 	generateServices(f.GetService(), content)
 	// Comments? unclear how to link them back to other elements
@@ -78,16 +138,11 @@ func generateFullFile(f *descriptorpb.FileDescriptorProto) (out *pluginpb.CodeGe
 	return
 }
 
-func generateDependencies(deps []string, content *strings.Builder) {
-	// FIXME: this pattern probably doesn't work at all for deps, I think we need to return structs that get imports added over time
-	for _, dep := range deps {
-		// TODO: get comment data somehow
-		comment := ""
-		name := filenameFromProto(dep)
-		content.WriteString(fmt.Sprintf("/** %s */\nimport {} from \"%s\";\n", comment, name.fullWithoutExtension))
+func generateDependencies(deps []string, pkgMap map[string]string, content *strings.Builder) {
+	if len(deps) == 0 {
+		return
 	}
-	content.WriteString("\n")
-	// content.WriteString("// TODO: imports\n\n")
+	content.WriteString("import * as packages from \"__packages__\";\n\n")
 }
 
 func generateEnums(enums []*descriptorpb.EnumDescriptorProto, content *strings.Builder) {
@@ -110,13 +165,13 @@ func generateEnums(enums []*descriptorpb.EnumDescriptorProto, content *strings.B
 	}
 }
 
-func generateMessages(messages []*descriptorpb.DescriptorProto, content *strings.Builder) {
+func generateMessages(messages []*descriptorpb.DescriptorProto, content *strings.Builder, pkgName string) {
 	for _, message := range messages {
 		// TODO: get comment data somehow
 		comment := "A message"
 		content.WriteString(fmt.Sprintf("/** %s */\nexport class %s extends Object {\n", comment, message.GetName()))
 		for _, field := range message.GetField() {
-			tsType := getNativeTypeName(field, message)
+			tsType := getNativeTypeName(field, message, pkgName)
 			// TODO: get comment data somehow
 			comment = "A field"
 			content.WriteString(fmt.Sprintf("	/** %s */\n	public %s?: %s;\n", comment, field.GetName(), tsType))
@@ -127,9 +182,9 @@ func generateMessages(messages []*descriptorpb.DescriptorProto, content *strings
 			if !nestedType.GetOptions().GetMapEntry() {
 				// TODO: get comment data somehow
 				comment = "A message"
-				content.WriteString(fmt.Sprintf("/** %s */\nexport class %s_%s extends Object {\n", comment, message.GetName(), nestedType.GetName()))
+				content.WriteString(fmt.Sprintf("/** %s */\nexport class %s__%s extends Object {\n", comment, message.GetName(), nestedType.GetName()))
 				for _, nestedField := range nestedType.GetField() {
-					tsType := getNativeTypeName(nestedField, nestedType)
+					tsType := getNativeTypeName(nestedField, nestedType, pkgName)
 					// TODO: get comment data somehow
 					comment = "A field"
 					content.WriteString(fmt.Sprintf("	/** %s */\n	public %s?: %s;\n", comment, nestedField.GetName(), tsType))
@@ -148,7 +203,7 @@ func generateComments(sourceCodeInfo *descriptorpb.SourceCodeInfo, content *stri
 
 }
 
-func getNativeTypeName(field *descriptorpb.FieldDescriptorProto, message *descriptorpb.DescriptorProto) string {
+func getNativeTypeName(field *descriptorpb.FieldDescriptorProto, message *descriptorpb.DescriptorProto, pkgName string) string {
 	switch field.GetType() {
 	case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE,
 		descriptorpb.FieldDescriptorProto_TYPE_FLOAT,
@@ -173,17 +228,24 @@ func getNativeTypeName(field *descriptorpb.FieldDescriptorProto, message *descri
 	case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
 		// TODO: this lookup is not efficient, but it'll do for now. building a map of known types by name as we go would be good
 		for _, nestedMessage := range message.GetNestedType() {
-			// FIXME: this *will* misfire at least sometimes, though we'll see if it particularly matters
+			// FIXME: it is possible for this to misfire at least sometimes, though we'll see if it particularly matters
 			if nestedMessage.GetOptions().GetMapEntry() && strings.Contains(field.GetTypeName(), nestedMessage.GetName()) {
-				keyType := getNativeTypeName(nestedMessage.GetField()[0], nil)
-				valType := getNativeTypeName(nestedMessage.GetField()[1], nil)
+				keyType := getNativeTypeName(nestedMessage.GetField()[0], nil, pkgName)
+				valType := getNativeTypeName(nestedMessage.GetField()[1], nil, pkgName)
 				return fmt.Sprintf("Map<%s, %s>", keyType, valType)
 			}
 		}
 		// Not a map
 		fallthrough
 	case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
-		return strings.ReplaceAll(strings.TrimPrefix(field.GetTypeName(), "."), ".", "_")
+		typeName := field.GetTypeName()
+		matches := packageReplacement.FindStringSubmatch(typeName)
+		if len(matches) != 3 {
+			panic(fmt.Errorf("type name did not match any valid pattern: %s, found %d instead of 3: %s", typeName, len(matches), matches))
+		}
+		pkgSection := fmt.Sprintf("packages.%s.", matches[1])
+		typeSection := strings.ReplaceAll(matches[2], ".", "__")
+		return fmt.Sprintf("%s%s", pkgSection, typeSection)
 	default:
 		panic(fmt.Errorf("unknown field type: %s", field))
 	}
