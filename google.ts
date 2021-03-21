a237316352e2163592859e0e56e01970a5ba3991
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-21 22:49:59 +1100

    Add build scripts and dependencies to top level files

---
+++ google.ts
@@ -0,0 +1,31 @@
+export class protobuf__Timestamp {
+
+}
+
+export class google__protobuf__Any {
+
+}
+
+export class google__protobuf__Duration {
+
+}
+
+export class google__protobuf__Struct {
+
+}
+
+export class google__protobuf__ListValue {
+
+}
+
+export class google__protobuf__Value {
+
+}
+
+export class google__protobuf__NullValue {
+
+}
+
+export class google__protobuf__Empty {
+
+}
