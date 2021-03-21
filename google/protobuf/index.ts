1e3cba1b9f52a190e6a0c6396490f91903c861ae
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-21 23:01:55 +1100

    Restructure exports some more

---
+++ google/protobuf/index.ts
@@ -0,0 +1,31 @@
+export class Timestamp {
+
+}
+
+export class Any {
+
+}
+
+export class Duration {
+
+}
+
+export class Struct {
+
+}
+
+export class ListValue {
+
+}
+
+export class Value {
+
+}
+
+export class NullValue {
+
+}
+
+export class Empty {
+
+}
