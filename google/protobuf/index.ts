1b58b6b3c1e7a85478f74bb366a0210afb7483ac
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-21 23:25:50 +1100

    Publish full google type implementations

--- google/protobuf/index.ts
+++ google/protobuf/index.ts
@@ -1,25 +1,25 @@
 export class Timestamp {
-
+	public value?: Date;
 }
 
 export class Any {
-
+	public value?: any;
 }
 
 export class Duration {
-
+	public seconds?: number;
 }
 
 export class Struct {
-
+	public value?: Object;
 }
 
 export class ListValue {
-
+	public value?: any[]
 }
 
 export class Value {
-
+	public value?: any;
 }
 
 export class Empty {
