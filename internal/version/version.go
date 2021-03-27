3064e4047e61fe6ca5bdec7726654b272334406a
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-27 22:57:20 +1100

    Bump version

--- internal/version/version.go
+++ internal/version/version.go
@@ -10,7 +10,7 @@ const (
 	// Major is the major version of the tool
 	Major = 0
 	// Minor is the minor version of the tool
-	Minor = 4
+	Minor = 5
 	// Patch is the patch version of the tool
 	Patch = 0
 )
